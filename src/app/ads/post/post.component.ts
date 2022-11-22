17f98dc63294c0ed3a5b2a104792538bfa130632
InheePark <59162676+InheePark@users.noreply.github.com>
2022-11-21 22:38:52 -0500

    test1

--- src/app/ads/post/post.component.ts
+++ src/app/ads/post/post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AdsService } from 'src/app/services/ads.service';
+import Ad from 'src/app/Models/ads.model';
+import User from 'src/app/Models/user.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  ad?: Ad;
+  user?: string = localStorage.getItem("username") ?? undefined;
 
-  constructor() { }
+  constructor(private adsService: AdsService, private route: ActivatedRoute) { }
+  postInfo = {
+    userName: "",
+    title: "", 
+    body: "", 
+    price: "", 
+    end: "", 
+    deliveryMethod: ""
+  }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get("id");
+    if(id === null){
+
+    }else{
+
+    }
+  }
+
+  post(): void{
+    
   }
 
 }
