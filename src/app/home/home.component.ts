d39e14e5e451cab9946d04b7edbbd3449510b97c
hanna050215 <hanna121979@gmail.com>
2022-11-23 14:31:01 +0900

    Homepage & QnA init

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Ad from '../Models/ads.model';
 import User from '../Models/user.model';
+import { AdsService } from '../services/ads.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -8,6 +10,15 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  ngOnInit(): void {}
-  constructor() {}
+  data: Ad[] = [];
+  ad?: Ad;
+  user?: User | undefined;
+  constructor(private adsService: AdsService, userService: UserService) { }
+
+  ngOnInit(): void {
+    this.adsService.getAds().subscribe(result => {
+
+    });
+  }
 }
+
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import Question from 'src/app/Models/question.model';
+import Ad from 'src/app/Models/ads.model';
+import { AdsService } from 'src/app/services/ads.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-questions',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./questions.component.css']
 })
 export class QuestionsComponent implements OnInit {
-
-  constructor() { }
+  data: Question[] = [];
+  ad?: Ad;
+  constructor(private adsServices: AdsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const adsId = this.route.snapshot.paramMap.get('adsId');
+    this.adsServices.getAd(adsId).subscribe(result => 
+        console.log(result.question))
+
   }
 
 }
