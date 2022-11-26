4d2836632ce12a980055b740118923763f2d8be1
Edmund Tang <98913678+EdmundLT@users.noreply.github.com>
2022-11-26 01:35:34 -0500

    deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  // apiUrl: "https://used-good-store-server.onrender.com/api"
-  apiUrl: "http://localhost:8000/api"
+  production: true,
+  apiUrl: "https://used-good-store-server.onrender.com/api"
+  // apiUrl: "http://localhost:8000/api"
 };
 
 /*
